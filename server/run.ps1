2e839ec96f6b4875c45d97c7416c36205ac4ba5a
gwy15 <gwy15thu@gmail.com>
2019-11-02 18:56:08 +0800

    Remove gunicorn, complete makefile

--- server/run.ps1
+++ server/run.ps1
@@ -1,2 +1,2 @@
 . venv/Scripts/activate.ps1
-python app.py --port 8081
+python src/app.py --port 8081
--- server/run.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-source ./venv/bin/activate
-gunicorn pushbot:asyncApp --worker-class aiohttp.GunicornWebWorker --bind unix:///tmp/wechat-push.sock
-
