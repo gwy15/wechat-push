1edf7e51bf7f94c642b4b50a670b2b9b47993527
gwy15 <gwy15thu@gmail.com>
2019-11-02 20:46:18 +0800

    compatible for python 3.6

--- server/src/wechat/tests/utils.py
+++ server/src/wechat/tests/utils.py
@@ -1,6 +1,15 @@
 import asyncio
 from functools import wraps
+import sys
 
+if sys.version_info >= (3,7):
+    run = asyncio.run
+else:
+    def run(awitable):
+        loop = asyncio.get_event_loop()
+        res = loop.run_until_complete(awaitable)
+        loop.close()
+        return res
 
 def async_test(asyncTestMethod):
     @wraps(asyncTestMethod)
